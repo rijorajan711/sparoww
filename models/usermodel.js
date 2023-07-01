9bc14245ad5860a6dcee53f963fb0dcd7c23a7e2
rijo <rijorajan27594@gamil.com>
2023-07-01 12:54:10 +0530

    recommit

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose") 
 const db = require("../config/connection")
  
-const userSchema = new mongoose.Schema({
+const userSchema= new mongoose.Schema({
     username:{
         type:String,
         required:true
