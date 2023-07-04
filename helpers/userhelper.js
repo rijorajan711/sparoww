387f76b626258d0639383bc0c3bf23f7a68db7ec
rijo <rijorajan27594@gamil.com>
2023-07-04 16:46:15 +0530

    last test url

--- helpers/userhelper.js
+++ helpers/userhelper.js
@@ -71,7 +71,7 @@ module.exports = {
         if (error) {
           console.log(error);
         } else {
-          console.log("Email sent: " + info.response);
+          console.log("Email is sent: " + info.response);
         }
         resolve();
       });
